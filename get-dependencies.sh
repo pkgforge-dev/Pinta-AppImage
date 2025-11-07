29ddd0ee6768e1cd191a69135fe70fdd2b0e5736
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-07 06:56:36 -0400

    Add files via upload

---
+++ get-dependencies.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -eu
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+echo "Installing build dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	base-devel        \
+	curl              \
+	git               \
+	libx11            \
+	libxrandr         \
+	libxss            \
+	pulseaudio        \
+	pulseaudio-alsa   \
+	wget              \
+	xorg-server-xvfb  \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-common --prefer-nano
+
+echo "Building pinta..."
+echo "---------------------------------------------------------------"
+sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+sed -i \
+	-e 's|-O2|-O3|'                              \
+	-e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|'  \
+	-e 's|#MAKEFLAGS|MAKEFLAGS|'                 \
+	/etc/makepkg.conf
+cat /etc/makepkg.conf
+
+git clone --depth 1 https://aur.archlinux.org/pinta.git ./pinta
+(
+	cd ./pinta
+	sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
+	makepkg -fs --noconfirm
+	ls -la ./
+	pacman --noconfirm -U ./*.pkg.tar.*
+)
+
+pacman -Q pinta | awk '{print $2; exit}' > ~/version

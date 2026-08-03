0a0a6b3748b4eff28bd98dcf87d488c591c7cf7b
Samueru-sama <xdglawyer@outlook.com>
2026-08-03 08:09:38 -0400

    build pinta as self contained dotnet binary

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH="$(uname -m)"
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm pinta
+pacman -Syu --noconfirm dotnet-sdk libadwaita
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -13,7 +13,59 @@ get-debloated-pkgs --add-common --prefer-nano
 
 echo "Building pinta..."
 echo "---------------------------------------------------------------"
-#make-aur-package PACKAGENAME
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+export DOTNET_NOLOGO=1
 
+git clone --depth 1 --branch 3.1.2 https://github.com/PintaProject/Pinta.git ./pinta-src && (
+	cd ./pinta-src
+	# The 3.1.2 release targets net8.0; match the Arch package by bumping to net10.0
+	sed -i 's/net8.0/net10.0/g' Directory.Build.props
+
+	# Mono.Addins uses reflection to (re)build its add-in database, which the
+	# trimmer strips -> root the addin/cecil assemblies so they stay intact
+	cat > Directory.Build.targets <<-'EOF'
+	<Project>
+	<ItemGroup>
+	<TrimmerRootAssembly Include="Mono.Addins" />
+	<TrimmerRootAssembly Include="Mono.Addins.Setup" />
+	<TrimmerRootAssembly Include="Mono.Addins.CecilReflector" />
+	<TrimmerRootAssembly Include="Mono.Cecil" />
+	<TrimmerRootAssembly Include="Mono.Cecil.Rocks" />
+	<TrimmerRootAssembly Include="Mono.Cecil.Mdb" />
+	<TrimmerRootAssembly Include="Mono.Cecil.Pdb" />
+	</ItemGroup>
+	</Project>
+	EOF
+
+	dotnet publish ./Pinta/Pinta.csproj \
+		-c Release                  \
+		-r linux-x64                \
+		--self-contained true       \
+		-p:PublishTrimmed=true      \
+		-p:TrimMode=partial         \
+		-p:BuildTranslations=true   \
+		-p:PublishDir=/usr/lib/pinta
+
+	# Dead weight, nothing references these
+	rm -f \
+		/usr/lib/pinta/Microsoft.CodeAnalysis.dll        \
+		/usr/lib/pinta/Microsoft.CodeAnalysis.CSharp.dll \
+		/usr/lib/pinta/Mono.Cecil.Mdb.dll                \
+		/usr/lib/pinta/Mono.Cecil.Pdb.dll                \
+		/usr/lib/pinta/Mono.Cecil.Rocks.dll
+
+	# dlopen'd lazily only when tracing, would drag in liblttng-ust
+	rm -f /usr/lib/pinta/libcoreclrtraceptprovider.so
+
+	mkdir -p /usr/share/pinta
+	cp -r /usr/lib/pinta/icons /usr/share/pinta
+	sed 's/^_//' xdg/com.github.PintaProject.Pinta.desktop.in > ./AppDir/Pinta.desktop
+	cp -r /usr/lib/pinta/locale/. /usr/share/locale/
+
+	awk -F'<|>' '/<Version>/{print $3; exit}' ./Directory.Build.props | sed 's/\.0$//' > ~/version
+)
+
+echo "Building libappstream stub..."
+echo "---------------------------------------------------------------"
 cc -shared -fPIC -O2 -o ./libappstream.so.5 -Wl,-soname,libappstream.so.5 libappstream-stub.c
 mv -v ./libappstream.so.5 /usr/lib
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q pinta | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -12,12 +11,13 @@ export DESKTOP=/usr/share/applications/com.github.PintaProject.Pinta.desktop
 export ICON=/usr/share/icons/hicolor/96x96/apps/com.github.PintaProject.Pinta.png
 export GTK_CLASS_FIX=1
 export DEPLOY_OPENGL=1
-export DEPLOY_DOTNET=1
+
+mkdir -p ./AppDir/bin
+cp -r /usr/lib/pinta/* ./AppDir/bin
+ln -s ./Pinta ./AppDir/bin/pinta
 
 # Deploy dependencies
-quick-sharun /usr/bin/pinta /usr/lib/pinta /usr/share/pinta
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/* /usr/share/pinta
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
